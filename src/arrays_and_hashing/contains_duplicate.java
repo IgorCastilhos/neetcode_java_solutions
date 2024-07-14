77ca367e8e4fe79a40e2d9c66e3874a60d29558e
igorc <igorcastilhos2010@hotmail.com>
2024-07-14 02:52:16 -0300

    Add: array & hashing/contains duplicate

---
+++ src/arrays_and_hashing/contains_duplicate.java
@@ -0,0 +1,16 @@
+package arrays_and_hashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class contains_duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
